6ca41cf94a1c47f27e335975c227fadb27427ff7
caseyst22 <ccstrayer02@gmail.com>
2023-11-18 11:15:40 -0600

    First commit

---
+++ Assets/BirdScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    public Rigidbody2D myRigidBody;
+    public float flapStrength;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) == true && birdIsAlive)
+        {
+            animator.SetBool("ButtonPressing", true);
+            myRigidBody.velocity = Vector2.up * flapStrength;
+            animator.SetTrigger("ButtonPress");
+            animator.SetBool("ButtonPressing", false);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
+    }
+}
---
+++ Assets/BottomOfScreenScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottomOfScreenScript : MonoBehaviour
+{
+    public LogicScript logic;
+    public BirdScript bird;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+        bird = GameObject.FindGameObjectWithTag("Bird").GetComponent<BirdScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        logic.gameOver();
+        bird.birdIsAlive = false;
+    }
+}
